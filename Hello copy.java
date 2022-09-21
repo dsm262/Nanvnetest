b8aa26a24ab4c51ed6b0768ae57d4ee8b7f43bef
dsm262 <91126289+dsm262@users.noreply.github.com>
2022-09-21 15:35:39 +0200

    added file

---
+++ Hello copy.java
@@ -0,0 +1,52 @@
+public class Hello {
+	public static void main(String args[]){
+		System.out.println("Hello WOrld");
+
+			/*final double pi = 3.14;
+			double r1;
+			double r2;
+			double r3;
+
+			r1 = 1;
+			r2 = 3;
+			r3 = 5;
+
+			System.out.println("r="+r1+"  -> area="+(pi*r1*r1));
+			System.out.println("r="+r2+"  -> area="+(pi*r2*r2));
+			System.out.println("r="+r3+"  -> area="+(pi*r3*r3));
+
+			System.out.println("r="+r1+"  -> C="+(2*pi*r1));
+			System.out.println("r="+r2+"  -> C="+(2*pi)*r2);
+			System.out.println("r="+r3+"  -> C="+(2*pi*r3));*/
+
+			/*final double day1 = 21.5;
+			final double day2 = 23.7;
+			final double day3 = 19.6;
+			final double day4 = 22.5;
+			final double day5 = 25.3;
+			final double day6 = 21.7;
+			final double day7 = 18.9;
+
+			System.out.println(day2-day1);
+			System.out.println(day3-day2);
+			System.out.println(day4-day3);
+			System.out.println(day5-day4);
+			System.out.println(day6-day5);
+			System.out.println(day7-day6);*/
+
+			/*int i = 42;
+			long l = 56;
+			double f = 3.14159;
+			double d = 3.14159*10;
+			System.out.printf("i=%d l=%,4d f=%f d=%6.2f", i, l, f, d);
+			*/
+
+
+			/*byte b = 127;
+			System.out.println(b);
+			b += 1;
+			System.out.println(b);
+			*/
+
+	}
+}
